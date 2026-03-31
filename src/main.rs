327faddf0aa05611747eec7dad80900913db08d7
Anon404 <ninjab467@gmail.com>
2026-03-31 14:33:40 +0000

    Initial commit: basic x11rb window manager

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use x11rb::{connect,connection::Connection};
+use x11rb::protocol::xproto::{EventMask,ConnectionExt,ChangeWindowAttributesAux};
+
+fn main() {
+    let (conn,screen_num) = connect(None).unwrap();
+    println!("Connected to X server {}",screen_num);
+
+    let screen = &conn.setup().roots[screen_num];
+    let root = screen.root;
+    println!("Root window id is {}",root);
+
+    conn.change_window_attributes(
+        root,
+        &ChangeWindowAttributesAux::new()
+        .event_mask(
+            EventMask::SUBSTRUCTURE_REDIRECT |
+            EventMask::SUBSTRUCTURE_NOTIFY |
+            EventMask::KEY_PRESS
+        )
+    ).unwrap();
+    conn.flush().unwrap();
+
+    println!("Done");
+
+    loop {
+        let event = conn.wait_for_event().unwrap();
+        println!("Event {:?}",event);
+    }
+}
